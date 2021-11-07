7d485f51b7c6ceb8f5c23b554b03e53b6b63dac7
tocchimittsu <arimagedon21@gmail.com>
2021-11-07 18:12:29 +0900

    修正

---
+++ main.py
@@ -0,0 +1,42 @@
+import cv2
+import sys
+import numpy as np
+
+# imread : 画像ファイルを読み込んで、多次元配列(numpy.ndarray)にする。
+# imreadについて : https://kuroro.blog/python/wqh9VIEmRXS4ZAA7C4wd/
+# 第一引数 : 画像のファイルパス
+# 戻り値 : 行 x 列 x 色の三次元配列(numpy.ndarray)が返される。
+img = cv2.imread('./sample.jpg')
+
+# 画像ファイルが正常に読み込めなかった場合、プログラムを終了する。
+if img is None:
+    sys.exit("Could not read the image.")
+
+# 第一引数(必須) : 多次元配列(画像情報)
+# 第二引数(必須) : 複数の座標。numpy.ndarray型。
+# 第三引数(必須) : 多角形の色を指定する。B(Blue)G(Green)R(Red)形式で指定する。tuple型。
+cv2.fillPoly(img, [np.array([(210, 200), (220, 300), (300, 340), (340, 220)])], (255, 0, 0))
+
+# imwrite : 画像の保存を行う関数
+# 第一引数 : 保存先の画像ファイル名
+# 第二引数 : 多次元配列(numpy.ndarray)
+# <第二引数の例>
+# [
+# [
+# [234 237 228]
+# ...
+# [202 209 194]
+# ]
+# [
+# [10 27 16]
+# ...
+# [36 67 46]
+# ]
+# [
+# [34 51 40]
+# ...
+# [50 81 60]
+# ]
+# ]
+# imwriteについて : https://kuroro.blog/python/i0tNE1Mp8aEz8Z7n6Ggg/
+cv2.imwrite('output.jpg', img)
